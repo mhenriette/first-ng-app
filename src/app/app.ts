0be2f179e665cd231d19b907a6363e4dcfaf6e12
mhenriette <mhenriette00@gmail.com>
2025-09-15 14:28:15 +0200

    feat: data binding

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,16 +1,25 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './components/header/header';
+import { Home } from './home/home';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header, Home],
   template: `
     <h1>Welcome to {{ title() }}!</h1>
-
+    <app-header />
+    <app-home />
     <router-outlet />
   `,
-  styles: [],
+  styles: [
+    `
+      h1 {
+        color: indigo;
+      }
+    `
+  ],
 })
 export class App {
-  protected readonly title = signal('first-ng-app');
+  protected readonly title = signal('My first Angular Application!!!!');
 }
