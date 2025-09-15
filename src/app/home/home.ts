42448092cb3cb6ac434a854a406732185b94ed53
mhenriette <mhenriette00@gmail.com>
2025-09-15 14:37:24 +0200

    feat: data binding between parent and child component

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { Greetings } from '../components/greetings/greetings';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [Greetings],
   templateUrl: './home.html',
   styleUrl: './home.scss'
 })
 export class Home {
+  parentGreetings= signal('this is the data from the parent component')
 
 }
